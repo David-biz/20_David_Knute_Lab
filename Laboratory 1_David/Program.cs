aeb21e566f6fb21dd9d06bafcda36f72d74efaa5
David_Mel <62503462+David-biz@users.noreply.github.com>
2020-11-29 20:08:32 +0200

    Add files via upload

---
+++ Laboratory 1_David/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLab {
+    class Program {
+        static void Main(string[] args) {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
